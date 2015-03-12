2cd57922055d64d9e856ee8707baf78714519b98
Elena <elenamanushyna@gmail.com>
2015-03-12 11:31:02 +0200

    Memento implemented

---
+++ DesignPatterns.Memento/Memento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Memento
+{
+    public class Memento
+    {
+        public Memento(string name, string phone, double budget)
+        {
+            this.Name = name;
+            this.Phone = phone;
+            this.Budget = budget;
+        }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public double Budget { get; set; }
+    }
+}
---
+++ DesignPatterns.Memento/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Memento
+{
+    /// <summary>
+    /// Without violating encapsulation, capture and externalize an objects internal state
+    /// so that the object can be restored to this state later.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var salesProspect = new SalesProspect();
+            salesProspect.Name = "Leo van Halen";
+            salesProspect.Phone = "(050) 000-0000";
+            salesProspect.Budget = 20000.0;
+
+            var prospectMemory = new ProspectMemory();
+            prospectMemory.Memento = salesProspect.SaveMemento();
+
+            salesProspect.Name = "Fred Welch";
+            salesProspect.Phone = "(099) 000-0000";
+            salesProspect.Budget = 40000.0;
+
+            salesProspect.RestoreMemento(prospectMemory.Memento);
+
+            Console.ReadLine();
+        }
+    }
+}
